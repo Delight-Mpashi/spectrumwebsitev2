13997c833ee55689265fb905286b4628be09c1e4
Delight-Mpashi <delight_mpashi@outlook.com>
2025-06-30 17:59:45 +0200

    Fix file path and remove duplicate loanforms route
    Corrected the file path for CSLoanForm.html in one route and removed a duplicate /loanforms route that served the wrong file. This ensures the correct HTML files are served for each loan form endpoint.

--- app.js
+++ app.js
@@ -39,9 +39,6 @@ app.get('/terms', (req, res) => {
 app.get('/loanforms/csloanform', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/pages/loanforms/asloanform.html'));
 });
-app.get('/loanforms', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/pages/loanforms/csloanform.html'));
-});
 
 app.get('/src/pages/loanforms/csloanform.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/loanforms/csloanform.html'));
@@ -56,7 +53,7 @@ app.get('/public/assets/images/:image', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/assets/images', req.params.image));
 });
 app.get('../src/pages/loanforms/CSLoanForm.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/loanforms/CSLoanForm.html'));
+  res.sendFile(path.join(__dirname, 'src/pages/loanforms/CSLoanForm.html'));
 });
 app.get('../src/pages/loanforms/ASLoanform.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/loanforms/ASLoanform.html'));
